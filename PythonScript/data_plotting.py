8364f77ad900c118641a28e41e4c819878eb279e
Diana Bolocan <DianaBolocan@users.noreply.github.com>
2019-11-03 21:19:00 +0200

    Added plots

--- PythonScript/data_plotting.py
+++ PythonScript/data_plotting.py
@@ -45,8 +45,7 @@ def compute_magnitude(matrix: list, window_interval: int):
     return result
 
 
-def process_data_walking(path: str, window_interval=100):
-    dataframe = pandas.read_csv(path)
+def process_data_walking(dataframe: pandas.DataFrame, window_interval=100):
     frequency_domain = numpy.fft.fftn(
         [dataframe['userAcceleration.x'], dataframe['userAcceleration.y'], dataframe['userAcceleration.z']]).T
     dataframe_size = dataframe.shape[0]
@@ -66,7 +65,7 @@ def process_data_walking(path: str, window_interval=100):
         raw_magnitudes.append(compute_magnitude(raw_slice.values.tolist(), window_interval))
         raw_average_peaks.append(peaks_count_distance(raw_slice.values.tolist())[0])
         raw_distance_peaks.append(peaks_count_distance(raw_slice.values.tolist())[1])
-        fft_slice = frequency_domain[index:index+100]
+        fft_slice = frequency_domain[index:index + 100]
         fft_means.append(list(numpy.mean(fft_slice, axis=0)))
         fft_medians.append(list(numpy.median(fft_slice, axis=0)))
         fft_magnitudes.append(compute_magnitude(fft_slice, window_interval))
@@ -78,23 +77,39 @@ def process_data_walking(path: str, window_interval=100):
     fft_means = numpy.array(fft_means)
     fft_medians = numpy.array(fft_medians)
     fft_magnitudes = numpy.array(fft_magnitudes)
+    return raw_means, raw_meadians, raw_magnitudes, raw_average_peaks, raw_distance_peaks, fft_means, fft_medians, fft_magnitudes
+
+
+def plot_acceleration(dataframe: pandas.DataFrame, file, save_to_dir: str, show_plot=True):
+    fig, axis = plt.subplots(3, figsize=(20, 10))
+    fig.suptitle(os.path.splitext(file)[0] + " acceleration x, y, x")
+    axis[0].plot(range(dataframe.shape[0]), dataframe['userAcceleration.x'])
+    axis[1].plot(range(dataframe.shape[0]), dataframe['userAcceleration.y'])
+    axis[2].plot(range(dataframe.shape[0]), dataframe['userAcceleration.z'])
+    plt.savefig(os.path.join(save_to_dir, os.path.splitext(file)[0] + " acceleration x, y, x.png"))
+    if show_plot:
+        plt.show()
+    plt.close()
     return
 
 
-def plot(directory: str, save_to_dir: str):
+def plot_all(directory: str, save_to_dir: str, show_plot=False):
     for subdir, dirs, files in os.walk(directory):
         for file in files:
             file_path = os.path.join(subdir, file)
             if "data_subjects_info" in file:
-                plot_subjects_info(file_path, save_to_dir, False)
+                plot_subjects_info(file_path, save_to_dir, show_plot)
             if "wlk" in subdir:
                 print(file_path)
-                process_data_walking(file_path)
-                exit(1)
-
+                dataframe = pandas.read_csv(file_path)
+                dir = os.path.split(subdir)[-1]
+                if not os.path.exists(os.path.join(save_to_dir, dir)):
+                    os.mkdir(os.path.join(save_to_dir, dir))
+                plot_acceleration(dataframe, file, os.path.join(save_to_dir, dir), show_plot)
+                process_data_walking(dataframe, 100)
     return
 
 
 if __name__ == '__main__':
     print("Plotting data")
-    plot(r"..\Dataset", r"..\Resources\Plots")
+    plot_all(r"..\Dataset", r"..\Resources\Plots")
