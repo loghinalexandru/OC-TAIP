2d97fa6f58ca9e8480ef54b38f51723109e90081
Alexandru Loghin <loghinalexandru61@gmail.com>
2019-12-02 16:48:27 +0200

    Add Small Optimization

--- ModelTrainingService/ModelTrainingService/Worker.cs
+++ ModelTrainingService/ModelTrainingService/Worker.cs
@@ -4,6 +4,8 @@ using ModelTrainingService.DataAccess;
 using ModelTrainingService.Helpers;
 using ModelTrainingService.Models;
 using System;
+using System.IO;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -15,7 +17,6 @@ namespace ModelTrainingService
         private readonly IStorageRepository _storageRepository;
         private readonly Options _options;
         private readonly IPythonHelper _helper;
-
 
         public Worker(ILogger<Worker> logger, Options options, IStorageRepository storageRepository,
             IPythonHelper helper)
@@ -33,10 +34,14 @@ namespace ModelTrainingService
                 _logger.LogInformation("Worker running at: {time}", DateTimeOffset.Now);
 
                 var users = await _storageRepository.GetUsers();
+                await _storageRepository.GetAllUserData("all-users.zip");
+
                 foreach (var user in users)
                 {
                     await TrainModelForUser(user.Username);
                 }
+
+                CleanDirectory();
 
                 await Task.Delay(TimeSpan.FromHours(_options.WorkerRefreshTime), stoppingToken);
             }
@@ -44,10 +49,17 @@ namespace ModelTrainingService
 
         private async Task TrainModelForUser(string username)
         {
-            await _storageRepository.GetAllUserData("all-users.zip");
             await _storageRepository.GetUserData(username, username + ".zip");
 
             _helper.RunScript();
+        }
+
+        private void CleanDirectory()
+        {
+            Directory
+                .GetFiles(".\\", "*.zip", SearchOption.TopDirectoryOnly)
+                .ToList()
+                .ForEach(File.Delete);
         }
     }
 }
