44458907d7b64d738bd880ba517f9a4b7c3d92f7
Diana Bolocan <DianaBolocan@users.noreply.github.com>
2019-12-02 19:53:53 +0200

    training

--- PythonScript/generate_model_train.py
+++ PythonScript/generate_model_train.py
@@ -1,12 +1,11 @@
-import keras
 import numpy as np
 from keras.models import Sequential
-from keras.layers import Dense, Activation, Dropout
+from keras.layers import Dense, Dropout
+from keras.utils import to_categorical
 import os
 import pickle
 import numpy
 import random
-
 
 SAVE_PATH = r"models"
 PROCESSED_DATA_DIR = r"processed_data"
@@ -23,7 +22,7 @@ class Model:
         return
 
     def compile(self):
-        self.model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+        self.model.compile(loss='binary_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
         return
 
     def train(self, x_train: np.array, y_train: np.array, x_validation: np.array, y_validation: np.array, batch_size=32,
@@ -37,36 +36,54 @@ class Model:
         return
 
 
-def split_train_validation(processed_data_dir: str, subjects: list, split_procentage=0.2):
+def get_numpy(processed_data_dir: str):
     x_data = None
     y_data = None
     for subdir, _, files in os.walk(processed_data_dir):
         subject = os.path.split(subdir)[-1]
-        if subject in subjects:
-            for file in files:
-                with open(os.path.join(subdir, file), "rb") as fd:
-                    saved_data = pickle.load(fd)
-                    if x_data is None:
-                        x_data = saved_data
-                    else:
-                        x_data = numpy.append(x_data, saved_data, axis=0)
-                    labels = numpy.repeat(int(subject), saved_data.shape[0])
-                    if y_data is None:
-                        y_data = labels
-                    else:
-                        y_data = numpy.append(y_data, labels)
-    validation_indeces = random.sample(range(x_data.shape[0]), int(split_procentage*x_data.shape[0]))
-    train_indeces = list(set(range(x_data.shape[0])) - set(validation_indeces))
-    train = zip(x_data[train_indeces],  y_data[train_indeces])
-    validation = zip(x_data[validation_indeces], y_data[validation_indeces])
-    return train, validation
+        for file in files:
+            with open(os.path.join(subdir, file), "rb") as fd:
+                saved_data = pickle.load(fd)
+                if x_data is None:
+                    x_data = saved_data
+                else:
+                    x_data = numpy.append(x_data, saved_data, axis=0)
+                labels = numpy.repeat(int(subject), saved_data.shape[0])
+                if y_data is None:
+                    y_data = labels
+                else:
+                    y_data = numpy.append(y_data, labels)
+    data = zip(x_data, y_data)
+    return data
+
+
+def train_models(data: numpy.array, save_to: str, procentage_validation=0.2):
+    input, output = zip(*data)
+    input = numpy.array(list(input))
+    output = numpy.array(list(output))
+
+    for subject in set(output):
+        print("Subject: {}".format(subject))
+        indexes = numpy.where(output == subject)[0]
+        indexes = numpy.append(indexes, random.sample(range(len(output)), len(indexes)))
+        indexes = numpy.unique(indexes)
+        x_train = input[indexes]
+        y_train = (output[indexes] == subject).astype(numpy.float)
+        y_train = y_train.T
+
+        indexes = random.sample(range(input.shape[0]), int(procentage_validation * input.shape[0]))
+        x_validation = input[indexes]
+        y_validation = (output[indexes] == subject).astype(numpy.float)
+        y_validation = y_validation.T
+
+        model = Model([64, 64, 1], 10)
+        model.compile()
+        model.train(x_train, y_train, x_validation, y_validation)
+        model.save(os.path.join(save_to, str(subject)+".h5"))
+    return
 
 
 if __name__ == '__main__':
     # train each model separately
-    train, validation = split_train_validation(PROCESSED_DATA_DIR, ['1', '2', '3', '4'])
-    x_train, y_train = zip(*train)
-    x_validation, y_validation = zip(*validation)
-    model = Model([64, 64, 1], 10)
-    model.compile()
-    model.train(x_train, y_train, x_validation, y_validation)
+    data = get_numpy(PROCESSED_DATA_DIR)
+    train_models(data, save_to=SAVE_PATH)
